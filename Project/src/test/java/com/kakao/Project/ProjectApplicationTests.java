24f51b6bcf2ce49ac5a5fcfe08b568f8347ee06c
JeongEun <JeongEun@DESKTOP-N602VP1>
2019-10-03 00:49:16 +0900

    Final Code

--- Project/src/test/java/com/kakao/Project/ProjectApplicationTests.java
+++ Project/src/test/java/com/kakao/Project/ProjectApplicationTests.java
@@ -9,18 +9,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
-import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kakao.project.controller.TransactionInqController;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ProjectApplicationTests {
